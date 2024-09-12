940d46f932d7fe090e3ce867f5569656964511a7
Zeks92 <zeks1992@gmail.com>
2024-09-12 14:20:44 +0200

    Added migrations,category controller and view

---
+++ BookStore/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BookStore.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace BookStore.Data
+{
+    public class ApplicationDbContext : DbContext
+    { 
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        { 
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
--- BookStore/Program.cs
+++ BookStore/Program.cs
@@ -1,7 +1,13 @@
+using BookStore.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options=>options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+ ));
 
 var app = builder.Build();
 
@@ -13,8 +19,8 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
+app.UseHttpsRedirection(); 
+app.UseStaticFiles(); 
 
 app.UseRouting();
 
