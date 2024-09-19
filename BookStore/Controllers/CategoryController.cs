f488080dfcc9c55f8689298370508584267a318a
Zeks92 <zeks1992@gmail.com>
2024-09-19 11:52:31 +0200

    Stared create category logic

--- BookStore/Controllers/CategoryController.cs
+++ BookStore/Controllers/CategoryController.cs
@@ -15,5 +15,10 @@ namespace BookStore.Controllers
             List<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 }
